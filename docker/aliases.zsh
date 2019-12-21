98860073c980e18fccb5b377c2bb24d651b1298c
Maxim Mikhaylov <lawnboymadmax@gmail.com>
2019-12-21 12:40:46 -0500

    add more docker aliases

--- docker/aliases.zsh
+++ docker/aliases.zsh
@@ -1,2 +1,9 @@
 alias d='docker $*'
-alias d-c='docker-compose $*'
+# Kill all running containers.
+alias dkillall='docker kill $(docker ps -q)'
+# Delete all stopped containers.
+alias dcleanc='printf "\n>>> Deleting stopped containers\n\n" && docker rm $(docker ps -a -q)'
+# Delete all untagged images.
+alias dcleani='printf "\n>>> Deleting untagged images\n\n" && docker rmi $(docker images -q -f dangling=true)'
+# Delete all stopped containers and untagged images.
+alias dclean='dcleanc || true && dleani'
