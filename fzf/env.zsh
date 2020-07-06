808478348be0a101cd759da5c8f3d54c7d112769
mmxmb <22385215+mmxmb@users.noreply.github.com>
2020-07-06 18:34:49 -0400

    add shell key bindings customization for fzf

--- fzf/env.zsh
+++ fzf/env.zsh
@@ -1,2 +1,6 @@
 # Use ~~ as the trigger sequence instead of the default **
 export FZF_COMPLETION_TRIGGER='~~'
+# show entries in directory when cd into directory
+export FZF_ALT_C_OPTS="--preview 'tree -C {} | head -200'" 
+# toggle full command preview (up to 5 lines) using ? key when doing history search
+export FZF_CTRL_R_OPTS="--preview 'echo {}' --preview-window down:5:hidden:wrap --bind '?:toggle-preview'"
