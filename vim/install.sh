99c4a1095dd20625ab5e69b70ce8a7fb8bfe3af7
mmxmb <22385215+mmxmb@users.noreply.github.com>
2020-12-09 11:58:00 +0000

    improve Vundle installation

--- vim/install.sh
+++ vim/install.sh
@@ -1,7 +1,9 @@
-if [ ! -d "~/.vim/bundle/Vundle.vim" ] 
+if [ -d ~/.vim/bundle/Vundle.vim ] 
 then
-  git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-else
+  echo "Update Vundle"
   git -C ~/.vim/bundle/Vundle.vim pull
+else
+  echo "Clone Vundle"
+  git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 fi
 vim +PluginInstall +qall
