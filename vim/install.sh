8fb891bfd13b4762c95df5bca0ed9ca307a16488
mmxmb <22385215+mmxmb@users.noreply.github.com>
2020-12-09 11:34:19 +0000

    Keep Vundle updated

--- vim/install.sh
+++ vim/install.sh
@@ -1,2 +1,7 @@
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+if [ ! -d "~/.vim/bundle/Vundle.vim" ] 
+then
+  git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+else
+  git -C ~/.vim/bundle/Vundle.vim pull
+fi
 vim +PluginInstall +qall
